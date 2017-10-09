708fae84425567a67986e64e9c7763b2759ae6e3
Kevin de Bree <kpdebree@gmail.com>
2017-10-08 23:42:01 -0700

    Update 7

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -1,10 +1,48 @@
 var map;
+var infowindow;
+var bounds;
+var markers = [];
 
+  	
+     
+	
 function initMap() {
-  	map = new google.maps.Map(document.getElementById('map'), {
-       	center: {lat: -34.397, lng: 150.644},
-        zoom: 8
-    });
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: new google.maps.LatLng(34.0522, -118.2437),
+		zoom: 8,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	});
+
+	for (var i = 0; i < locations.length; i++) {
+		var location = locations[i].location;
+		var name = locations[i].name;
+		var id = locations[i].id;
+		var distance = locations[i].distance;
+
+		var marker = new google.maps.Marker({
+			position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+			title: name,
+			id: id,
+			distance: distance,
+			// animation: google.maps.Animation.DROP
+		});
+		console.log(marker)
+
+		markers.push(marker);
+
+		marker.addListener('click', function(){
+        	infoWindowInit(this, createInfoWindow());
+        });
+
+        marker.addListener('mouseover', function(){
+            this.setIcon(hoverMarker);
+        });
+
+        marker.addListener('mouseout', function(){
+            this.setIcon(defaultMarker);
+        });
+	}
+	console.log(markers)
 }
 
 function mapError() {
