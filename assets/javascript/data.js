ed4c3f8dbb2edd7a8d722bc3b8771f4a5ef388b6
kpdebree <kpdebree@gmail.com>
2017-10-11 22:02:29 -0700

    Update

--- assets/javascript/data.js
+++ assets/javascript/data.js
@@ -1,42 +1,78 @@
 var locations = [
     {
         name: "Joshua Tree",
-        location: {lat: 33.8734, lng: -115.9010},
+        lat: 33.8734, 
+        lng: -115.9010,
         distance: "distance_stub",
         difficulty: "difficulty_stub",
-        photo: "photo_stub",
+        photo: "assets/images/JoshuaTree.jpg",
         id: 1
     },
     {
         name: "Mount Pinos",
-        location: {lat: 34.8128, lng: -119.1454},
+        lat: 34.8128, 
+        lng: -119.1454,
         distance: "distance_stub",
         difficulty: "difficulty_stub",
-        photo: "photo_stub",
+        photo: "assets/images/MtPinos2.jpg",
         id: 2
     },
     {
         name: "Solstice Canyon",
-        location: {lat: 34.0741, lng: -118.1804},
+        lat: 34.0338, 
+        lng: -118.7416,
         distance: "distance_stub",
         difficulty: "difficulty_stub",
-        photo: "photo_stub",
+        photo: "assets/images/solstice-canyon.jpg",
         id: 3
     },
     {
         name: "Los Liones Trail",
-        location: {lat: 34.2364, lng: -117.6590},
+        lat: 34.0466, 
+        lng: -118.5588,
         distance: "distance_stub",
         difficulty: "difficulty_stub",
-        photo: "photo_stub",
+        photo: "assets/images/LosLiones.jpg",
         id: 4
     },
     {
         name: "Santa Ynez Trailhead",
-        location: {lat: 34.3586, lng: -117.7645},
+        lat: 34.0771, 
+        lng: -118.5657,
         distance: "distance_stub",
         difficulty: "difficulty_stub",
-        photo: "photo_stub",
+        photo: "assets/images/SantaYnez.jpg",
         id: 5
+    },
+    {
+        name: "Mount Hollywood",
+        lat: 34.135730, 
+        lng: -118.304268,
+        distance: "distance_stub",
+        difficulty: "difficulty_stub",
+        photo: "assets/images/mtHollywood.jpg",
+        id: 6
+    },
+    {
+        name: "Mount Baldy",
+        lat: 34.2364, 
+        lng: -117.6590,
+        distance: "distance_stub",
+        difficulty: "difficulty_stub",
+        photo: "assets/images/MtBaldy.jpg",
+        id: 7
+    },
+    {
+        name: "Mount Baden-Powell",
+        lat: 34.3586, 
+        lng: -117.7645,
+        distance: "distance_stub",
+        difficulty: "difficulty_stub",
+        photo: "assets/images/BadenPowell.jpg",
+        id: 8
     }
 ];
+
+$(document).ready(function() { 
+    console.log("Data Ready")
+});
--- assets/javascript/utils.js
+++ assets/javascript/utils.js
@@ -1,6 +1,36 @@
 var CLIENT_ID = "JMRL0LMRP5KL2BKOT5MJGGADP4FGW1RB1NDY53ENOVEN5KR3";
 var CLIENT_SECRET = "1D32DJ1AOTCPXDGR4VFWYQTFR2DEE24NYS1YQV41SLFGUBNL";
 var location;
-var query = 'trails';
-var base_url = 'https://api.foursquare.com/v2/venues/explore?near=' + location + '&query=' + query + '&venuePhotos=1';
-var query_url = base_url + '&v=20170101' + '&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET;
+var current_date = new Date();
+var dateString = current_date.toISOString().slice(0,10).replace(/-/g,"");
+
+
+
+
+
+// var infoUrl = "https://api.foursquare.com/v2/venues/search?client_id=X3JOZJUYLFFFB22HKOYSX0FSX30LZFP0DRPUE2E2WP04MMFU&client_secret=I0L2CGALFX1S5DC0NWEUGKNMUUUTKHGGJXKPB2YSQJWBBDTB&section=trails&ll="
+//             + String(location.lat) + "," + String(location.lng) + "&v=" + dateStr + "&query=" + marker.title;
+
+function getDataTest() {
+	location = locations[1];
+
+	var query_url = 'https://api.foursquare.com/v2/venues/search?' + '&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET +
+	'&section=trails&ll=' + String(location.lat) + ',' + String(location.lng) + '&v=' + dateString +'&query=' + location.title;
+
+	$.ajax({
+		type: "GET",
+        url: query_url,
+        dataType: "json",
+        success: function(data) {
+            console.log(data)
+            var venue = data.response.venues[0];
+            var venueInfo = {
+                name: location.title,
+                location: venue.location.city + ", " + venue.location.state,
+                checkins: venue.stats.checkinsCount,
+                length: location.distance,
+                id: data.response.venues[0].id
+            };
+		}
+	});
+}
